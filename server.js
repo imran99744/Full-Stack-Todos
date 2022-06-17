303dab1613c1696bb9e6fdd8247b701d9d94e154
MD AL IMRAN <anjuarabegum1978@gmail.com>
2022-06-17 21:14:33 +0600

    modify something

--- server.js
+++ server.js
@@ -14,12 +14,18 @@ app.get("/todo-list", (req, res) => {
   res.send(todos);
 });
 
+app.get("/", (req, res) => {
+  res.send("Server is on");
+});
+
 // app.post("/add-item", (req, res) => {
 //   console.log(req.body.item);
 //   todos.push(req.body.item);
-//   res.status(201).send(data);
+//   res.status(201).send(todos);
 // });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
 });
